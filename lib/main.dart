1c8ac003cc5f6f91b5bb478f212fc9abc28c3a29
Chhim Sonita <chhimsonita@key-3.local>
2024-11-11 00:08:21 +0700

    w4-s3

--- lib/main.dart
+++ lib/main.dart
@@ -7,44 +7,78 @@ void main() => runApp(MaterialApp(
           backgroundColor: Colors.blue,
           title: const Text("Favorite cards"),
         ),
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(width: 1, color: Colors.grey)),
-              ),
-              padding: EdgeInsets.all(20),
-              // YOUR CODE
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Expanded(
-                    flex: 6,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'title',
-                          style: TextStyle(color: Colors.blue, fontSize: 16),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('description'),
-                      ],
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () => {},
-                    icon: const Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+        body: const CardList(),
       ),
     ));
+
+class CardList extends StatelessWidget {
+  const CardList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CardContainer(),
+        CardContainer(),
+        CardContainer(),
+      ],
+    );
+  }
+}
+
+class CardContainer extends StatefulWidget {
+  const CardContainer({
+    super.key,
+  });
+
+  @override
+  State<CardContainer> createState() => _CardContainerState();
+}
+
+class _CardContainerState extends State<CardContainer> {
+  bool onPressed = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(bottom: BorderSide(width: 1, color: Colors.grey)),
+      ),
+      padding: EdgeInsets.all(20),
+      // YOUR CODE
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Expanded(
+            flex: 6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'title',
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text('description'),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: onPressed ? Colors.red: Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                onPressed = !onPressed;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
